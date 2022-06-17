b3ca67df28abbeec852acff150a34721532e68c1
Curtis <ar06053127hg@gmail.com>
2022-06-17 18:22:38 +0800

    add enable_click_time

--- autoKey/main.py
+++ autoKey/main.py
@@ -1,10 +1,31 @@
 import pyautogui
 import time
+import datetime
+
+
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
+
+
+def enable_click_time():
+    morning_start = datetime.time(9, 0, 0, 0)
+    morning_end = datetime.time(12, 0, 0, 0)
+    afternoon_start = datetime.time(13, 0, 0, 0)
+    afternoon_end = datetime.time(18, 0, 0, 0)
+
+    now = datetime.datetime.now().time()
+    enable = time_in_range(morning_start, morning_end, now) | time_in_range(afternoon_start, afternoon_end, now)
+
+    return enable
 
 
 def mouse_left_click_demo():
-    delay_time = 4 * 60 + 30
-    # current_mouse_X, current_mouse_Y = pyautogui.position()
+    delay_time = 4 * 60
+
     target_mouse_X, target_mouse_Y = pyautogui.position()
 
     key = input('press key \'p\', move the mouse and press ENTER to set the position:')
@@ -13,11 +34,14 @@ def mouse_left_click_demo():
         print(target_mouse_X, target_mouse_Y)
 
     while True:
-        current_mouse_X, current_mouse_Y = pyautogui.position()
-        pyautogui.click(target_mouse_X, target_mouse_Y)
-        print("%s" % time.ctime())
-        print('mouse left click')
-        pyautogui.moveTo(current_mouse_X, current_mouse_Y)
+        if enable_click_time():
+            current_mouse_X, current_mouse_Y = pyautogui.position()
+            pyautogui.click(target_mouse_X, target_mouse_Y)
+            print('mouse left click')
+            pyautogui.moveTo(current_mouse_X, current_mouse_Y)
+        else:
+            pass
+        print(time.ctime())
         time.sleep(delay_time)
 
 
